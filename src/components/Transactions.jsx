18af3d443bf4e23004173a8c323e2f4fc66ee8d9
Christine <you@example.com>
2023-09-26 20:05:31 +0800

    Updating Transactions features

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -70,48 +70,64 @@ const Transactions = (props) => {
       </div>
 
       {activeTab === "deposits" && (
-        <div className="depositTransactionsHistory">
-          <p className="forDepositOption">Channel</p>
-          <p className="forDepositDate">Date of Transaction</p>
-          <p className="forNameOfSender">Sender</p>
-          <p className="forDepositAmount">Amount</p>
-          <p className="forTransactionsStatus">Status</p>
+        <div>
+          <div className="transactionsTitleContainer">
+            <h3 className="depositsTransactionsTitle">Deposits History</h3>
+          </div>
+          <div className="depositsTransactionsContainer">
+            <p className="forDepositOption">Channel</p>
+            <p className="forDepositDate">Date of Transaction</p>
+            <p className="forNameOfSender">Sender</p>
+            <p className="forDepositAmount">Amount</p>
+            <p className="forTransactionsStatus">Status</p>
+          </div>
         </div>
       )}
 
       {activeTab === "withdrawals" && (
-        <div className="withdrawTransactionsHistory">
-          <h3 className="withdrawTransactionsTitle">Withdrawals History</h3>
-          <p className="forWithdrawOption">Channel</p>
-          <p className="forWithdrawDate">Date of Transaction</p>
-          <p className="forWithdrawAmount">Amount</p>
-          <p className="forWithdrawStatus">Status</p>
-          <div className="changeWithdrawStatus">
-            <select
-              key="changeStatus"
-              id="changeStatus"
-              value={withdrawStatus}
-              onChange={(e) =>
-                handleWithdrawStatusChange(e, transaction.id)
-              } /*{handleWithdrawStatusChange}*/
-            >
-              <option value="">Change Transaction Status</option>
-              <option value="Successful">Successful</option>
-              <option value="Cancel">Cancelled</option>
-            </select>
+        <div>
+          <div className="transactionsTitleContainer">
+            <h3 className="withdrawTransactionsTitle">Withdrawals History</h3>
+          </div>
+          <div className="transactionsContainer">
+            <p className="forWithdrawOption">Channel</p>
+            <p className="forWithdrawDate">Date</p>
+            <p className="forWithdrawAmount">Amount</p>
+            <p className="forWithdrawStatus">Status</p>
+            <p className="forWithdrawStatus">Change Status</p>
+            <div className="changeWithdrawStatus">
+              <select
+                key="changeStatus"
+                id="changeStatus"
+                value={withdrawStatus}
+                onChange={(e) =>
+                  handleWithdrawStatusChange(e, transaction.id)
+                } /*{handleWithdrawStatusChange}*/
+              >
+                <option value="">Select</option>
+                <option value="Successful">Successful</option>
+                <option value="Cancel">Cancelled</option>
+              </select>
+            </div>
           </div>
         </div>
       )}
 
       {activeTab === "sendMoney" && (
-        <div className="sendMoneyTransactionsHistory">
-          <h3 className="sendMoneyTransactionsTitle">Send Money History</h3>
-          <p className="forSendMoneyOption">Account Number</p>
-          <p className="forSendMoneyRecipientName">Recipient</p>
-          <p className="forSendMoneyRecipientAcccountNumber">Account Number</p>
-          <p className="forSendMoneyDate">Date of Transaction</p>
-          <p className="forSendMoneyAmount">Amount</p>
-          <p className="forSendMoneyStatus">Status</p>
+        <div>
+          <div className="transactionsTitleContainer">
+            <h3 className="sendMoneyTransactionsTitle">Send Money History</h3>
+          </div>
+          <div className="sendMoneyTransactionsContainer">
+            <p className="forSendMoneyOption">Channel</p>
+            <p className="forSendMoneyRecipientName">Recipient</p>
+            <p className="forSendMoneyRecipientAcccountNumber">
+              Account Number
+            </p>
+            <p className="forSendMoneyDate">Date</p>
+            <p className="forSendMoneyAmount">Amount</p>
+            <p className="forSendMoneyStatus">Status</p>
+          </div>
         </div>
       )}
 
