3f06f966a8c4b12365bb88bc6a3ca5f7d12af10c
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-05-28 17:18:16 +0800

    Added br to h1.

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 const WelcomePage = () => {
   return (
     <div className="welcomePageContainer">
-      <h1 className="welcomeMessage">Welcome to Prosperity Bank</h1>
+      <h1 className="welcomeMessage">
+        Welcome to <br /> Prosperity Bank
+      </h1>
       <h4 className="howToHelpMessage">How can we help you today?</h4>
       <nav>
         <ul>
